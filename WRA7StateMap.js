93518922e34b271815295988888d5c2bfa598f87
WRAMarComm <100541126+WRAMarComm@users.noreply.github.com>
2022-02-27 18:14:05 -0700

    Add files via upload

--- WRA7StateMap.js
+++ WRA7StateMap.js
@@ -1,5 +1,8 @@
 Highcharts.maps["WRAStates"]= {
-    "type": "FeatureCollection",
+    "title": "WRAStates",
+    "type":"FeatureCollection", 
+    "crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:EPSG::2169"}},
+    "hc-transform":{"default":{"crs":"+proj=tmerc +lat_0=49.83333333333334 +lon_0=6.166666666666667 +k=1 +x_0=80000 +y_0=100000 +ellps=intl +towgs84=-189.681,18.3463,-42.7695,-0.33746,-3.09264,2.53861,0.4598 +units=m +no_defs"}},
     "features": [
     {
     "type": "Feature",
